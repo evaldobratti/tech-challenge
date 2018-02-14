db17c1a1cbb6d7b2e0cf0598bcee05071190f7af
Evaldo Bratti <evaldo.bratti@gmail.com>
2018-02-14 12:44:45 -0200

    typo

--- README.md
+++ README.md
@@ -42,7 +42,7 @@ Dado que qualquer operação feita no sistema respeita essa estrutura, é possí
 ```
 system = FinancialSystem.create("system")
 ```
-# Criando uma conta no sistema financeiro
+# Criando uma conta
 As contas podem ter um limite para ficarem negativas, que é determina pelo terceiro parametro.
 Quando este limite é zero, a conta não pode ficar negativa.
 ```
@@ -50,7 +50,7 @@ Quando este limite é zero, a conta não pode ficar negativa.
 {:ok, system, acc_clark} = FinancialSystem.add_account(system, "Clark Kent", zero_usd)
 {:ok, system, acc_frank} = FinancialSystem.add_account(system, "Frank Castle", zero_usd)
 ```
-# Depositando dinheiro
+# Depositando
 ```
 assert FinancialSystem.balance(system, acc_bruce) == zero_usd
 
@@ -58,7 +58,7 @@ assert FinancialSystem.balance(system, acc_bruce) == zero_usd
 
 assert FinancialSystem.balance(system, acc_bruce) == ten_usd
 ```
-# Sacando dinheiro
+# Sacando
 ```
 {:ok, five_usd} = Money.create(5, usd)
 
@@ -70,7 +70,7 @@ assert FinancialSystem.balance(system, acc_clark) == zero_usd
 {:error, "No sufficient funds"} = FinancialSystem.withdraw(system, acc_clark, five_usd)
 assert FinancialSystem.balance(system, acc_clark) == zero_usd
 ```
-# Transferindo dinheiro
+# Transferindo
 ```
 assert FinancialSystem.balance(system, acc_bruce) == five_usd
 assert FinancialSystem.balance(system, acc_clark) == zero_usd
@@ -82,8 +82,8 @@ negative_five_usd = Money.negative(five_usd)
 assert FinancialSystem.balance(system, acc_bruce) == negative_five_usd
 assert FinancialSystem.balance(system, acc_clark) == ten_usd
 ```
-# Cambio
-### Depósito
+# Câmbio
+### Depositando
 ```
 assert FinancialSystem.balance(system, acc_bruce) == negative_five_usd
 
@@ -91,7 +91,7 @@ assert FinancialSystem.balance(system, acc_bruce) == negative_five_usd
 
 assert FinancialSystem.balance(system, acc_bruce) == zero_usd
 ```
-### Saque
+### Sacando
 ```
 assert FinancialSystem.balance(system, acc_clark) == ten_usd
 
@@ -102,7 +102,7 @@ assert FinancialSystem.balance(system, acc_clark) == five_usd
 {:ok,  after_exchange} = Money.create(16.65, brl)
 assert {_, _, {_, ^after_exchange}} = transaction
 ```
-### Transferencia
+### Transferindo
 ```
 assert FinancialSystem.balance(system, acc_clark) == five_usd
 
@@ -123,13 +123,16 @@ assert FinancialSystem.balance(system, acc_clark) == four_usd
 assert FinancialSystem.balance(system, acc_bruce) == zero_usd
 assert FinancialSystem.balance(system, acc_frank) == zero_usd
 
-{:ok, system, _} = FinancialSystem.transfer(system, acc_clark, [acc_bruce, acc_frank], four_usd)
+{:ok, money_usd} = Money.create(1.33, usd)
+{:ok, system, _} = FinancialSystem.transfer(system, acc_clark, [acc_bruce, acc_frank], money_usd)
 
-{:ok, two_usd} = Money.create(2, usd)
+{:ok, bigger_part} = Money.create(0.67, usd)
+{:ok, smaller_part} = Money.create(0.66, usd)
+{:ok, balance} = Money.create(2.67, usd)
 
-assert FinancialSystem.balance(system, acc_clark) == zero_usd
-assert FinancialSystem.balance(system, acc_frank) == two_usd
-assert FinancialSystem.balance(system, acc_bruce) == two_usd
+assert FinancialSystem.balance(system, acc_clark) == balance
+assert FinancialSystem.balance(system, acc_bruce) == bigger_part
+assert FinancialSystem.balance(system, acc_frank) == smaller_part
 ```
 
 Todo o código existente nesse readme se encontra no teste test/playground_test.exs
--- test/playground_test.exs
+++ test/playground_test.exs
@@ -18,19 +18,19 @@ defmodule PlaygroundTest do
     # Criando um sistema financeiro
     system = FinancialSystem.create("system")
 
-    # Criando uma conta no sistema financeiro
+    # Criando uma conta
     {:ok, system, acc_bruce} = FinancialSystem.add_account(system, "Bruce Wayne", ten_usd)
     {:ok, system, acc_clark} = FinancialSystem.add_account(system, "Clark Kent", zero_usd)
     {:ok, system, acc_frank} = FinancialSystem.add_account(system, "Frank Castle", zero_usd)
 
-    # Depositando dinheiro
+    # Depositando
     assert FinancialSystem.balance(system, acc_bruce) == zero_usd
 
     {:ok, system, _} = FinancialSystem.deposit(system, acc_bruce, ten_usd)
 
     assert FinancialSystem.balance(system, acc_bruce) == ten_usd
 
-    # Sacando dinheiro
+    # Sacando
     {:ok, five_usd} = Money.create(5, usd)
 
     assert FinancialSystem.balance(system, acc_bruce) == ten_usd
@@ -41,7 +41,7 @@ defmodule PlaygroundTest do
     {:error, "No sufficient funds"} = FinancialSystem.withdraw(system, acc_clark, five_usd)
     assert FinancialSystem.balance(system, acc_clark) == zero_usd
 
-    # Transferindo dinheiro
+    # Transferindo
     assert FinancialSystem.balance(system, acc_bruce) == five_usd
     assert FinancialSystem.balance(system, acc_clark) == zero_usd
 
@@ -52,15 +52,15 @@ defmodule PlaygroundTest do
     assert FinancialSystem.balance(system, acc_bruce) == negative_five_usd
     assert FinancialSystem.balance(system, acc_clark) == ten_usd
 
-    # Cambio
-    ### Depósito
+    # Câmbio
+    ### Depositando
     assert FinancialSystem.balance(system, acc_bruce) == negative_five_usd
 
     {:ok, system, _} = FinancialSystem.deposit_exchange(system, acc_bruce, ten_brl, 0.5)
 
     assert FinancialSystem.balance(system, acc_bruce) == zero_usd
 
-    ### Saque
+    ### Sacando
     assert FinancialSystem.balance(system, acc_clark) == ten_usd
 
     {:ok, system, transaction} = FinancialSystem.withdraw_exchange(system, acc_clark, five_usd, brl, 3.33)
@@ -70,7 +70,7 @@ defmodule PlaygroundTest do
     {:ok,  after_exchange} = Money.create(16.65, brl)
     assert {_, _, {_, ^after_exchange}} = transaction
 
-    ### Transferencia
+    ### Transferindo
     assert FinancialSystem.balance(system, acc_clark) == five_usd
 
     {:ok, one_usd} = Money.create(1, usd)
@@ -89,13 +89,16 @@ defmodule PlaygroundTest do
     assert FinancialSystem.balance(system, acc_bruce) == zero_usd
     assert FinancialSystem.balance(system, acc_frank) == zero_usd
 
-    {:ok, system, _} = FinancialSystem.transfer(system, acc_clark, [acc_bruce, acc_frank], four_usd)
+    {:ok, money_usd} = Money.create(1.33, usd)
+    {:ok, system, _} = FinancialSystem.transfer(system, acc_clark, [acc_bruce, acc_frank], money_usd)
 
-    {:ok, two_usd} = Money.create(2, usd)
+    {:ok, balance} = Money.create(2.67, usd)
+    {:ok, bigger_part} = Money.create(0.67, usd)
+    {:ok, smaller_part} = Money.create(0.66, usd)
 
-    assert FinancialSystem.balance(system, acc_clark) == zero_usd
-    assert FinancialSystem.balance(system, acc_frank) == two_usd
-    assert FinancialSystem.balance(system, acc_bruce) == two_usd
+    assert FinancialSystem.balance(system, acc_clark) == balance
+    assert FinancialSystem.balance(system, acc_bruce) == bigger_part
+    assert FinancialSystem.balance(system, acc_frank) == smaller_part
 
   end
 end
